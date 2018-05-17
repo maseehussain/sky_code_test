6fb8c1ba31837c6c800bd6f71193863f35a22aad
Ma5ee2 <hussainmasee@gmail.com>
2018-05-17 11:51:22 +0100

    styled the logo

--- src/components/SkyAssets.js
+++ src/components/SkyAssets.js
@@ -17,14 +17,14 @@ class SkyAssets extends React.Component {
     return (
       <div className="row">
         <div className="page-banner col-md-12">
-          <img className="Sky-logo" src="https://www.venturethree.com/assets/site/work/sky/_1024xAUTO_crop_center-center/sky-fallback2.png"/>
+          <img className="sky-logo" src="https://www.venturethree.com/assets/site/work/sky/_1024xAUTO_crop_center-center/sky-fallback2.png"/>
           <div className="index">
             {this.state.assets.map(asset => {
               return (
                 <div key={asset.assetImage} className="image-tile col-md-4 col-sm-6 col-xs-12">
                   <h1 className="asset-name">{asset.name}</h1>
                   <h5 className="asset-provider">{asset.provider}</h5>
-                  <img src={asset.assetImage} className="img-responsive"/>
+                  <img src={asset.assetImage} className="asset-image img-responsive"/>
                   <h2 className="asset-description">{asset.description}</h2>
                   <h4 className="skygo-views">Sky Go views: {asset.skygoTotalViews}</h4>
                   <h4 className="nowtv-views">NowTV views: {asset.nowtvTotalViews}</h4>
